9d2f333aba2c686843250443216356107c0aa155
herrera396 <gabriel.herrera@gloablhitss.com.br>
2024-05-09 12:17:36 -0300

    new Libs

--- app.js
+++
@@ -1,7 +0,0 @@
-const app = require('./config/serverConfig')
-
-const PORT = 3009
-
-app.listen(PORT, function () {
-  console.log(`Servidor Express - OK - Porta:${PORT}`)
-})
--- app/controllers/indexController.js
+++
@@ -1,6 +0,0 @@
-console.log('INDEX CONTROLLER - OK')
-
-/// ///////////////// GET ////////////////////
-module.exports.index = function (req, res) {
-  res.send(200, "Hello Test7!")
-}
